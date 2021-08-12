29434da187f320c7be1f04fec9af8c245fe90d61
Sean Campbell <sean.campbell3161@gmail.com>
2021-08-12 16:27:58 -0500

    finish calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0) return 0;
+	return array.reduce((acc,cv) => acc + cv, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, cv) => acc * cv);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0) 
+    return -1;
+
+  else if(num === 0)
+    return 1;
+
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 module.exports = {
